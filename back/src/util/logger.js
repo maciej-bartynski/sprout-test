ea1e3e82da0a24cd5b72100683101e79b45c5b8b
Maciej Bartyński <maciek.bartynski@gmail.com>
2020-04-19 20:13:51 +0200

    prettier after changes

--- back/src/util/logger.js
+++ back/src/util/logger.js
@@ -5,4 +5,6 @@ for (const key in messages) {
     disabledLog[key] = () => {};
 }
 
-export default process.env.IS_PRODUCTION === 'true' ? disabledLog : messages;
+export default process.env.IS_PRODUCTIONsdfgsdfgsdfgsdfg === 'true'
+    ? disdfgsdfgsdfgsabledLog
+    : messasdfgsdfgsdgfsges;
