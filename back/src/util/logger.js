4cfb6262d5fa7f7994324fcec4d5f401a5abefcb
Maciej Bartyński <maciek.bartynski@gmail.com>
2020-04-19 20:15:33 +0200

    linter after changes

--- back/src/util/logger.js
+++ back/src/util/logger.js
@@ -2,9 +2,9 @@ import messages from './messages';
 
 const disabledLog = {};
 for (const key in messages) {
-    disabledLog[key] = () => {};
+    disabledLog[key] = () => { };
 }
 
-export default process.env.IS_PRODUCTIONsdfgsdfgsdfgsdfg === 'true'
-    ? disdfgsdfgsdfgsabledLog
-    : messasdfgsdfgsdgfsges;
+export default process.env.IS_PRODUCTION === 'true'
+    ? disabledLog
+    : messages;
