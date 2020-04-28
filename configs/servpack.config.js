6b83f2ec2dfe2e594a25ad5455ce8eb888c9b257
Maciej Bartyński <maciek.bartynski@gmail.com>
2020-04-28 12:20:52 +0200

    missing packages in root sprout

--- configs/servpack.config.js
+++ configs/servpack.config.js
@@ -1,13 +1,23 @@
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const { useBuildspath, usePublicpath, useBackendapi } = require('../sprout-frontend/server');
+
 const { build } = useBuildspath;
 const { static, public, documents } = usePublicpath;
 const { api } = useBackendapi;
 
+const usePackages = Object.freeze({
+    bodyParser: bodyParser.json(),
+    cookieParser: cookieParser(),
+    compression: compression()
+});
+
 const config = {
-    port: 5060,
-    domain: "localhost:5060",
+    port: 5000,
+    domain: "localhost:5000",
     protocol: 'https',
     certification: {
         autogen: false,
@@ -27,6 +37,11 @@ const config = {
         ],
         router: api
     },
+    middlewares: [
+        usePackages.bodyParser,
+        usePackages.cookieParser,
+        usePackages.compression,
+    ],
 }
 
 module.exports = config;
